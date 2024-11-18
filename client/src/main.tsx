296e1b7aa5154e5e7ac73681a8d7ae91f1683e3f
alibaba0010 <zakariyahali100@gmail.com>
2024-11-18 21:38:37 +0100

    Working with client react, react-router, tailwind

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThirdwebProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThirdwebProvider>
   </React.StrictMode>
 );
