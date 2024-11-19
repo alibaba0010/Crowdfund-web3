ee50c1422cbb745c9891619ba9830de01fc33ebe
alibaba0010 <zakariyahali100@gmail.com>
2024-11-19 23:13:57 +0100

    Adding new packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import Routes, Route from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
+import { Sidebar, Navbar } from "./components";
+import { Home, Profile, CreateCampaign, CampaignDetails } from "./pages";
 import "./App.css";
 
 function App() {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative">{ <Sidebar /> }</div>
+      <div className="sm:flex hidden mr-10 relative">{<Sidebar />}</div>
 
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
