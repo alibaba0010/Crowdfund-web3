9dd72af4b0d4fd9e6fd1a236a6f07472c6eb1239
alibaba0010 <zakariyahali100@gmail.com>
2024-11-19 22:45:57 +0100

    Modifying index and sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
-// import Routes, Route from "react-router-dom";
+import Routes, Route from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 relative">{/* <Sidebar /> */}</div>
+      <div className="sm:flex hidden mr-10 relative">{ <Sidebar /> }</div>
 
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        {/* <Navbar />
+        <Navbar />
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-campaign" element={<CreateCampaign />} />
           <Route path="/campaign-details/:id" element={<CampaignDetails />} />
-        </Routes> */}
+        </Routes>
       </div>
     </div>
   );
