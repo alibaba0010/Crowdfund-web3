a116a37afa3686763d7d4072ef857f0bfc33cc3f
alibaba0010 <zakariyahali100@gmail.com>
2024-11-20 21:12:44 +0100

    Working with client debugging issues with nodemodules

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,32 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import { DisplayCampaigns } from '../components';
-import { useStateContext } from '../context'
+import { DisplayCampaigns } from "../components";
+import { useStateContext } from "../context";
+
+// const Profile = () => {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [campaigns, setCampaigns] = useState([]);
+
+//   const { address, contract, getUserCampaigns } = useStateContext();
+
+//   const fetchCampaigns = async () => {
+//     setIsLoading(true);
+//     const data = await getUserCampaigns();
+//     setCampaigns(data);
+//     setIsLoading(false);
+//   }
+
+//   useEffect(() => {
+//     if(contract) fetchCampaigns();
+//   }, [address, contract]);
+
+//   return (
+//     <DisplayCampaigns
+//       title="All Campaigns"
+//       isLoading={isLoading}
+//       campaigns={campaigns}
+//     />
+//   )
+// }
 
 const Profile = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [campaigns, setCampaigns] = useState([]);
+  return <div>Profile</div>;
+};
 
-  const { address, contract, getUserCampaigns } = useStateContext();
-
-  const fetchCampaigns = async () => {
-    setIsLoading(true);
-    const data = await getUserCampaigns();
-    setCampaigns(data);
-    setIsLoading(false);
-  }
-
-  useEffect(() => {
-    if(contract) fetchCampaigns();
-  }, [address, contract]);
-
-  return (
-    <DisplayCampaigns 
-      title="All Campaigns"
-      isLoading={isLoading}
-      campaigns={campaigns}
-    />
-  )
-}
-
-export default Profile
+export default Profile;
