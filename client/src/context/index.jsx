0cb0ecc8b7bfdc6e469b0b91b6fc749a64af34f8
alibaba0010 <zakariyahali100@gmail.com>
2024-11-24 12:45:10 +0100

    Working with thirdweb to interact with the smart contract

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,17 +1,27 @@
-import React, { useContext, createContext } from "react";
+import { useContext, createContext } from "react";
 
-// import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
 import { ethers } from "ethers";
-// import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
 
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  // const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
-  // const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+  const { contract } = useContract(
+    "0x51AFcbD12376434B68dD826802049a9634cb2364"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
 
-  // const address = useAddress();
-  // const connect = useMetamask();
+  const address = useAddress();
+  const connect = useMetamask();
 
   const publishCampaign = async (form) => {
     try {
