d61fa0e7e09497bfb23a5705e5e1fc90d98754de
alibaba0010 <zakariyahali100@gmail.com>
2024-11-27 12:47:53 +0100

    working with the create campaign method

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,14 +1,18 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import { createWallet, injectedProvider } from "thirdweb/wallets";
-import {} from "thirdweb/react";
-import { createThirdwebClient } from "thirdweb";
+import {
+  createThirdwebClient,
+  prepareContractCall,
+  getContract,
+  sendTransaction,
+} from "thirdweb";
 import { clientId } from "../utils";
-import { getContract } from "thirdweb";
 import { sepolia } from "thirdweb/chains";
 const StateContext = createContext();
 
 const client = createThirdwebClient({ clientId });
+
 const StateContextProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [wallet, setWallet] = useState("");
@@ -22,7 +26,6 @@ const StateContextProvider = ({ children }) => {
     // the contract's address
     address: "0x51AFcbD12376434B68dD826802049a9634cb2364",
   });
-  console.log("Contract: " + JSON.stringify(contract));
 
   const connectWallet = async () => {
     const metamaskProvider = injectedProvider("io.metamask");
@@ -53,16 +56,25 @@ const StateContextProvider = ({ children }) => {
       }
 
       // Assuming you have a contract instance available
-      const data = await contract.call("createCampaign", [
-        form.target,
-        form.title,
-        form.description,
-        new Date(form.deadline).getTime(),
-      ]);
-      console.log("Data: ", data);
+      const transaction = prepareContractCall({
+        contract,
+        method:
+          "function createCampaign(uint256 targetAmount, uint256 deadline, string title, string description) payable returns (uint256)",
+        params: [
+          form.target,
+          new Date(form.deadline).getTime(),
+          form.title,
+          form.description,
+        ],
+      });
+      const { transactionHash } = await sendTransaction({
+        transaction,
+        account,
+      });
+      console.log("Data: ", transactionHash);
 
-      console.log("Campaign created successfully:", data);
-      return data;
+      console.log("Campaign created successfully:", transactionHash);
+      return transaction;
     } catch (error) {
       console.error("Failed to publish campaign:", error);
       throw error;
