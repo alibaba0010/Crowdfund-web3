897f68271d9896612332f1f90ef3f9b076e86c38
alibaba0010 <zakariyahali100@gmail.com>
2024-11-27 12:09:32 +0100

    Done with connecting account moving to creating a campaign-- writing to the contract

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,9 +1,11 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import { createWallet, injectedProvider } from "thirdweb/wallets";
-import { useConnect, useActiveWallet } from "thirdweb/react";
+import {} from "thirdweb/react";
 import { createThirdwebClient } from "thirdweb";
 import { clientId } from "../utils";
+import { getContract } from "thirdweb";
+import { sepolia } from "thirdweb/chains";
 const StateContext = createContext();
 
 const client = createThirdwebClient({ clientId });
@@ -11,13 +13,25 @@ const StateContextProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [wallet, setWallet] = useState("");
 
+  // get a contract
+  const contract = getContract({
+    // the client you have created via `createThirdwebClient()`
+    client,
+    // the chain the contract is deployed on
+    chain: sepolia,
+    // the contract's address
+    address: "0x51AFcbD12376434B68dD826802049a9634cb2364",
+  });
+  console.log("Contract: " + JSON.stringify(contract));
+
   const connectWallet = async () => {
     const metamaskProvider = injectedProvider("io.metamask");
-    console.log("Connecting wallet: ", metamaskProvider);
+    // console.log("Connecting wallet: ", metamaskProvider);
     if (!metamaskProvider) {
       alert("Please install the metamsk extension");
       return;
     }
+
     const metamaskWallet = createWallet("io.metamask");
 
     const account = await metamaskWallet.connect({
@@ -39,12 +53,13 @@ const StateContextProvider = ({ children }) => {
       }
 
       // Assuming you have a contract instance available
-      const data = await wallet.contract.call("createCampaign", [
+      const data = await contract.call("createCampaign", [
         form.target,
         form.title,
         form.description,
         new Date(form.deadline).getTime(),
       ]);
+      console.log("Data: ", data);
 
       console.log("Campaign created successfully:", data);
       return data;
